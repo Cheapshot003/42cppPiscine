e4f2c5e65beaf48a0390b3bee397bfcb828be8b8
dercheapi <dercheapi@protonmail.com>
2023-06-08 17:21:03 +0200

    ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
-#include <string>
 
-
+int main(int argc, char **argv)
+{
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISES *\n";
+  }
+  else
+  {  
+    int i = 1;
+    int j = 0;
+    while (i < argc)
+    {
+      j = 0;
+      while (argv[i][j])
+      {
+        if (argv[i][j] >= 97 && argv[i][j] <= 122)
+          argv[i][j] -= 32;
+        std::cout << argv[i][j];
+        j++;
+      }
+      i++;
+    }
+    std::cout << std::endl;
+    
+  }
+  return (0);
+}
